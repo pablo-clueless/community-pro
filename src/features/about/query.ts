5a2f0b1b54d065d557e4b608cc5b5083b6f4033a
Samson Okunola <smsnmicheal@gmail.com>
2024-06-29 14:24:51 +0100

    feature: added team members

--- src/features/about/query.ts
+++ src/features/about/query.ts
@@ -1,29 +1,10 @@
 import axios from "axios"
 
-type User = {
-	login: string
-	id: number
-	node_id: string
-	avatar_url: string
-	gravatar_id: string
-	url: string
-	html_url: string
-	followers_url: string
-	following_url: string
-	gists_url: string
-	starred_url: string
-	subscriptions_url: string
-	organizations_url: string
-	repos_url: string
-	events_url: string
-	received_events_url: string
-	type: string
-	site_admin: boolean
-}
+import { GithubUserProps } from "@/types"
 
 const GetOrgMembers = async (org: string, page?: number) => {
 	return axios
-		.get<User[]>(`https://api.github.com/orgs/${org}/members?page=${page}`)
+		.get<GithubUserProps[]>(`https://api.github.com/orgs/${org}/members?page=${page}`)
 		.then((res) => res.data)
 }
 
--- src/features/teams/members/components/card.tsx
+++ src/features/teams/members/components/card.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const Card = ({ image, name }: Props) => {
 	return (
-		<div className="flex flex-col items-center gap-4">
+		<div className="flex aspect-square w-[110px] flex-col items-center gap-4">
 			<div className="relative !z-[1] aspect-square w-[75px] rounded-full border border-white bg-white before:absolute before:-right-1 before:top-0 before:h-full before:w-full before:rounded-full before:bg-primary-100">
 				<Image
 					src={image}
